7dbbad45433c2df430e7487d2e882b32ddb29b6a
Adithya-v-2004 <adithyav25042004@gmail.com>
2024-07-17 15:51:57 +0530

    Update and rename Task 1 to Task 1.py

---
+++ Task 1.py
@@ -0,0 +1,30 @@
+def caesar_encrypt(text, shift):
+    result = ""
+    for char in text:
+        if char.isalpha():
+            ascii_offset = 65 if char.isupper() else 97
+            result += chr((ord(char) - ascii_offset + shift) % 26 + ascii_offset)
+        else:
+            result += char
+    return result
+
+def caesar_decrypt(text, shift):
+    result = ""
+    for char in text:
+        if char.isalpha():
+            ascii_offset = 65 if char.isupper() else 97
+            result += chr((ord(char) - ascii_offset - shift) % 26 + ascii_offset)
+        else:
+            result += char
+    return result
+
+text = "HELLO WORLD!"
+shift = 3
+
+encrypted_text = caesar_encrypt(text, shift)
+print("Encrypted text:", encrypted_text)
+
+decrypted_text = caesar_decrypt(encrypted_text, shift)
+print("Decrypted text:", decrypted_text)
+
+
